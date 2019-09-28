f41a8152bf3f0031e8936150726f90c05f3adbb8
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-09-29 00:25:47 +0530

    classifier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Matrix from './brainchip.js/Matrix';
+import TeamClassifier from './brainchip.js/TeamClassifier';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    let m = new Matrix(3, 2);
+    console.table(m.matrix);
+  }
+
+  render() {
+    return (
+      <div>
+        <TeamClassifier />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/brainchip.js/TeamClassifier.js
@@ -0,0 +1,151 @@
+import { range } from 'ramda';
+import React, { Component } from 'react';
+
+class Circle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+class TeamClassifier {
+  high = 400;
+  low = 0;
+
+  randomPoints = range(0, 100).map(_ => {
+    let x = Math.random() * (this.high - this.low) + this.low;
+    let y = Math.random() * (this.high - this.low) + this.low;
+    return new Circle(x, y);
+  });
+
+  weights = {
+    x: Math.random(),
+    y: Math.random() * -1
+  };
+
+  error = null;
+
+  actual_team = point => {
+    return point.x > point.y ? 1 : -1;
+  };
+
+  newPoints = () => {
+    this.randomPoints = range(0, 100).map(_ => {
+      let x = Math.random() * (this.high - this.low) + this.low;
+      let y = Math.random() * (this.high - this.low) + this.low;
+      return new Circle(x, y);
+    });
+  };
+
+  train = () => {
+    this.randomPoints.map(point => {
+      const guessResult = this.guess(point);
+      const error = this.actual_team(point) - guessResult;
+
+      this.error = error;
+
+      this.weights = {
+        x: this.weights.x + point.x * error,
+        y: this.weights.y + point.y * error
+      };
+    });
+  };
+
+  sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+
+    while (currentTime + miliseconds >= new Date().getTime()) {}
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  trainSlow = update => {
+    this.randomPoints.map(async point => {
+      await this.sleep(800);
+      const guessResult = this.guess(point);
+      const error = this.actual_team(point) - guessResult;
+
+      this.error = error;
+
+      this.weights = {
+        x: this.weights.x + point.x * error,
+        y: this.weights.y + point.y * error
+      };
+      update();
+    });
+  };
+
+  guess = point => {
+    if (!point) {
+      point = {
+        x: 220,
+        y: 300
+      };
+    }
+    const weights = {
+      x: this.weights.x,
+      y: this.weights.y
+    };
+    const sum = point.x * weights.x + point.y * weights.y;
+    const team = sum > 0 ? 1 : -1;
+    return team;
+  };
+}
+
+export default class TeamClassifierView extends Component {
+  tc = new TeamClassifier();
+  X_MAX = 400;
+  Y_MAX = 400;
+
+  handleTrain = event => {
+    this.tc.train();
+    this.forceUpdate();
+  };
+  handleTrainSlow = event => {
+    const update = () => {
+      this.forceUpdate();
+    };
+    this.tc.trainSlow(update);
+  };
+
+  handleNewPoints = event => {
+    this.tc.newPoints();
+    this.forceUpdate();
+  };
+
+  render() {
+    return (
+      <div className='tc-root'>
+        <svg className='tc-svg' height={this.X_MAX} width={this.Y_MAX}>
+          {this.tc.randomPoints.map((point, idx) => (
+            <circle
+              key={idx}
+              cx={point.x}
+              cy={point.y}
+              r='3'
+              fill={this.tc.guess(point) === 1 ? 'red' : 'blue'}
+            />
+          ))}
+          <line x1='0' x2='400' y1='0' y2='400' stroke='#999' />
+        </svg>
+        <p>NOTE: Train till weights become almost like X == Y</p>
+        <button className='tc-button' onClick={this.handleTrain}>
+          Train
+        </button>
+        <button className='tc-button' onClick={this.handleNewPoints}>
+          Random points with trained weights
+        </button>
+        <button className='tc-button' onClick={this.handleTrainSlow}>
+          See visual training of weights
+        </button>
+        <div>
+          <h4>Weights</h4>
+          <p>X : {this.tc.weights.x}</p>
+          <p>Y : {this.tc.weights.y}</p>
+          <p>Last Error : {this.tc.error}</p>
+        </div>
+      </div>
+    );
+  }
+}
